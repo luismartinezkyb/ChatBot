f453931cae29f4fd36fb115b309dfec57d533709
Luis Martinez <89945446+luismartinezkyb@users.noreply.github.com>
2022-05-12 23:37:01 -0500

    Update lincebot.py
    Commit actualización de mensaje de bienvenida y recibido por parte del chatBot

--- lincebot.py
+++ lincebot.py
@@ -3,8 +3,14 @@ from telegram.ext import (Updater, CommandHandler)
 def start(update, context):
 	''' START '''
 	# Enviar un mensaje a un ID determinado.
-    print("enviado")
-	context.bot.send_message(update.message.chat_id, "Bienvenido")
+    
+
+	context.bot.send_message(update.message.chat_id, '''!Hola lince \U0001F44B \U0001F44B!, soy un ChatBot(Lincebot) \U0001F916 \n\n
+		Seré tu guía virtual de las instalaciones del Tecnológico Nacional de México en Celaya Campus 2. \U0001F431 \U0001F431 \n\n
+		Para ayudarte a encontrar el edificio que gustes puedes preguntarme/escribirme de la siguiente manera: \"¿Edificio -Tu destino-?\". \U0001F6B6 \U0001F6B6 \n\n
+		Ejemplos: ¿Edificio Sistemas?, ¿Edificio Cafetería?, ¿Edificio Asociaciones?\U0001F3EB  \n\n
+		Espero pueda ayudarte a encontrar tu destino, Orgullosamente Linces! \U0001F9BE \U0001F431''')
+print("Mensaje Enviado con exito!")
 
 def main():
 	TOKEN="5355902687:AAFfQtANKiNyGc7bd8QIy98Qz8dURToJ-mw"
